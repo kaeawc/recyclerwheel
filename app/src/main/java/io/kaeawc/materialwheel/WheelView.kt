9d7e7b57839b1410fd663cd6341a32fba152b48e
Jason Pearson <jason.d.pearson@gmail.com>
2017-03-10 15:48:45 -0500

    spacing

--- app/src/main/java/io/kaeawc/materialwheel/WheelView.kt
+++ app/src/main/java/io/kaeawc/materialwheel/WheelView.kt
@@ -3,7 +3,6 @@ package io.kaeawc.materialwheel
 import android.support.v7.widget.RecyclerView
 import android.view.View
 
-
 abstract class WheelView<in T>(view: View) : RecyclerView.ViewHolder(view) {
 
     abstract fun applyWheel(totalHeight: Int, colors: Array<Int>, animate: Boolean): Boolean
